968060120920d6647b8e6ffbd2ce1c7e97041681
Derek Griffin <griffinderek6@gmail.com>
2022-08-14 15:28:44 -0600

    finished 3 and 4

---
+++ MethodsWithLoopsAndConditionalsEx3and4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Write a method to accept two integers as parameters
+//and check whether they are equal or not
+
+//Console.WriteLine(counterTwo(2, 8));
+
+//static bool counterTwo(int a, int b)
+//{
+//    var check = (a == b) ? true : false;
+
+//    return check;
+//}
+
+
+//Write a method to check whether a
+//given number is even or odd
+
+int myAge = 31;
+if(myAge % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
